d6ebe23a3c0f74415ceb62825a8f894015b202f5
Amit Mandal <138322339+amit-mandal-kgce@users.noreply.github.com>
2023-11-25 01:36:48 +0530

    second

---
+++ src/components/Footer.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Link from 'next/link'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { IoLogoGithub } from 'react-icons/io'
+
+const Footer = () => {
+  return (
+    <footer className='mt-8' style={{ backgroundImage: `url(footer.svg)` }}>
+      <div className="grid md:grid-cols-2 gap-4 p-3 text-white">
+        <div className="flex flex-col  md:flex-row justify-between items-center space-y-3 md:space-x-4">
+            <div className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r px-10 from-pink-400 to-green-400"><p>IndMart</p></div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 space-y-5">
+                <div className="flex flex-col space-y-2">
+                    <h1 className='font-bold'>Product</h1>
+                    <Link href={''}>Demo</Link>
+                    <Link href={''}>Pricing</Link>
+                    <Link href={''}>Roadmap</Link>
+                    <Link href={''}>Security FAQ</Link>
+                    <Link href={''}>Features</Link>
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <h1 className='font-bold'>Company</h1>
+                    <Link href={''}>About Us</Link>
+                    <Link href={''}>Careers</Link>
+                    <Link href={''}>Press</Link>
+                    <Link href={''}>Support</Link>
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <h1 className='font-bold'>Resource</h1>
+                    <Link href={''}>Installotion Manual</Link>
+                    <Link href={''}>Release Notes</Link>
+                    <Link href={''}>Community Help</Link>
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-col p-3 space-y-3">
+            <div className="font-bold">Contact with us</div>
+            <div className="flex space-x-3">
+                <FaFacebook/>
+                <FaTwitter/>
+                <FaInstagram/>
+                <IoLogoGithub/>
+            </div>
+            <div className="font-bold">Stay Up to date with passbolt</div>
+            <div className="text-white">Elevate your style effortlessly with our curated collection of the latest trends. Discover fashion that speaks to you and technology that enhances your lifestyle. Your satisfaction is our top priority, and we're here to make every shopping experience a joyful one.</div>
+            <div className="flex flex-col sm:flex-row space-y-3 sm:space-x-2">
+                <input type="text" placeholder='Enter your mail' className='px-2 outline-none'/>
+                <div className="">
+                    <Link href='' className='px-3 py-1 border-2 border-white rounded-md'>Suscribe</Link>
+                </div>
+            </div>
+        </div>
+      </div>
+      <h1 className="text-white text-center py-4">
+            Created by <span className="text-accent">AMIT</span> | &copy; 2023
+            All rights reserved.
+          </h1>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,76 @@
+"use client"
+import Link from 'next/link'
+import React, {useState} from 'react'
+import { IoIosSearch } from "react-icons/io";
+import { FaRegUserCircle, FaShoppingCart  } from "react-icons/fa";
+import { MdSegment } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+
+function NavBar() {
+
+  const [showbox, setShowBox] = useState(false)
+  const toggle = () => {
+    setShowBox(!showbox);
+  }
+
+  return (
+    <div>
+      <nav className="bg-white shadow-xl h-12 py-2 px-3 flex justify-between items-center">
+        <div className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-green-600">IndMart</div>
+        <div className='hidden md:block'>
+          <div className="flex space-x-4 font-semibold">
+          <Link href="/">Home</Link>
+          <Link href="/men">Men</Link>
+          <Link href="/women">Women</Link>
+          <Link href="/beauty">Beauty</Link>
+          <Link href="/studio">Studio</Link>
+        </div>
+        </div>
+        
+        <div className="border py-1 px-3 rounded-full hidden md:block">
+          <div className="flex space-x-3 items-center">
+            <input type="text" className='outline-none pl-2'/>
+            <IoIosSearch />
+          </div>
+        </div>
+        
+        <div className="hidden md:block">
+          <div className="flex space-x-4 text-xl">
+          <FaRegUserCircle />
+          <FaShoppingCart />
+        </div>
+        </div>
+        
+        <div className="justify-end md:hidden text-2xl pr-2" onClick={toggle}>
+          {showbox ? <RxCross2 />:<MdSegment />}
+        </div>
+      </nav>
+
+      <div className="flex flex-col px-4 space-y-4 bg-white shadow-lg">
+        {showbox &&
+        (<div className="flex flex-col space-y-4 font-semibold">
+          <Link onClick={toggle} href="/">Home</Link>
+          <Link onClick={toggle} href="/men">Men</Link>
+          <Link onClick={toggle} href="/women">Women</Link>
+          <Link onClick={toggle} href="/beauty">Beauty</Link>
+          <Link onClick={toggle} href="/studio">Studio</Link>
+        </div>)}
+        {showbox && (<div className="border py-1 px-3 rounded-full">
+          <div className="">
+            <div className="flex space-x-3 items-center">
+            <input type="text" className='outline-none pl-2 w-full'/>
+            <IoIosSearch onClick={toggle}/>
+          </div>
+          </div>
+        </div>)}
+        {showbox && (<div className="flex space-x-4 text-xl py-4" onClick={toggle}>
+          <FaRegUserCircle />
+          <FaShoppingCart />
+        </div>)}
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
+
