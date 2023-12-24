dc39ddb96d6ec93f16daccc98580cc28cecf8070
Amit Mandal <138322339+amit-mandal-kgce@users.noreply.github.com>
2023-12-24 20:09:43 +0530

    delet env file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+// module.exports = {
+//   experimental: {
+//     pageDataCollectionTimeout: 1000 * 60 * 5, // 5 minutes (adjust as needed)
+//   },
+//   // Other configuration options...
+// };
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     pageDataCollectionTimeout: 1000 * 60 * 5, // 5 minutes (adjust as needed)
+//   },
+//   // Other configuration options...
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-module.exports = {
-  experimental: {
-    pageDataCollectionTimeout: 1000 * 60 * 5, // 5 minutes (adjust as needed)
-  },
+const nextConfig = {
   // Other configuration options...
 };
+
+module.exports = nextConfig;
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,20 +1,48 @@
+// import mongoose from "mongoose";
+
+// export async function connectToMongoDB(){
+//   try {
+//     mongoose.connect(process.env.MONGO_URI!)
+//     const connection = mongoose.connection
+
+//     connection.on('connected', () => {
+//       console.log('connectDB')
+//     })
+
+//     connection.on('error', (err) => {
+//       console.log('cannot connectDB' + err)
+//       process.exit()
+//     })
+//   } catch (error) {
+//     console.log('erroe DB :>>' + error)
+//   }
+// }
+
 import mongoose from "mongoose";
 
-export async function connectToMongoDB(){
+export async function connectToMongoDB() {
   try {
-    mongoose.connect(process.env.MONGO_URI!)
-    const connection = mongoose.connection
+    const mongoURI = process.env.MONGO_URI?.toString();
+
+    if (!mongoURI) {
+      console.error('MONGO_URI is not defined');
+      process.exit(1); // Exit the process with an error code
+    }
+
+    await mongoose.connect(mongoURI);
+
+    const connection = mongoose.connection;
 
     connection.on('connected', () => {
-      console.log('connectDB')
-    })
+      console.log('Connected to MongoDB');
+    });
 
     connection.on('error', (err) => {
-      console.log('cannot connectDB' + err)
-      process.exit()
-    })
+      console.error('Cannot connect to MongoDB: ' + err);
+      process.exit(1); // Exit the process with an error code
+    });
   } catch (error) {
-    console.log('erroe DB :>>' + error)
+    console.error('Error connecting to MongoDB: ' + error);
+    process.exit(1); // Exit the process with an error code
   }
 }
-
